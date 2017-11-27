af8e2402849cbbc792761794fc625600ca9009a1
Hirotoshin <hirotoshin0211@gmail.com>
2017-11-27 23:25:44 +0900

    kansei?

--- practice_musicApp/ViewController.swift
+++ practice_musicApp/ViewController.swift
@@ -12,23 +12,19 @@ import AVFoundation
 class ViewController: UIViewController {
 
     @IBAction func cymbal(_ sender: Any) {
-        do{
-        cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint:nil)
-        cymbalPlayer.play()
-        }catch{
-            print("エラー：シンバル")
-        }
+        soundPlayer(&cymbalPlayer, path: cymbalPath, count: 0)
     }
     
     @IBAction func guitar(_ sender: Any) {
-        do {
-            guitarPlayer = try AVAudioPlayer(contentsOf:guitarPath, fileTypeHint:nil)
-            guitarPlayer.play()
-        }catch{
-            print("エラー：ギター")
-        }
+        soundPlayer(&guitarPlayer, path: guitarPath, count: 0)
+    }
+    @IBAction func play(_ sender: Any) {
+        soundPlayer(&backmusicPlayer, path: backmusicPath, count: -1)
     }
     
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -44,6 +40,23 @@ class ViewController: UIViewController {
     let guitarPath = Bundle.main.bundleURL.appendingPathComponent("tw069.mp3")
     var guitarPlayer = AVAudioPlayer()
 
+    
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("btn03.mp3")
+    var backmusicPlayer = AVAudioPlayer()
+    
+    
+    fileprivate func soundPlayer(_ player:inout AVAudioPlayer, path:URL, count:Int){
+        do{
+        player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+        player.numberOfLoops = count
+        player.play()
+    } catch {
+        print("error")
+        }
+    }
 
+
+    
 }
 
